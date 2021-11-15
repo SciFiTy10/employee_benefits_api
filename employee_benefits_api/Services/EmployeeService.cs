1714588f6b5ee8983d35ae848f3b841e0f6c725b
Tyler <tythedeveloper10@gmail.com>
2021-11-15 12:04:50 -0600

    update service method

--- employee_benefits_api/Services/EmployeeService.cs
+++ employee_benefits_api/Services/EmployeeService.cs
@@ -19,31 +19,39 @@ namespace employee_benefits_api.Services
 
         public async Task<EmployeeList> CreateEmployee(Employee employee)
         {
-            //set up the connection
-            using var connection = new SqliteConnection($"Data Source={databaseConfig.Name}");
-            //insert the employee and get the id generated from it's insertion
-            var result = await connection.ExecuteScalarAsync<int>("INSERT INTO Employee (FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip)" +
-                "VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @AddressLine1, @AddressLine2, @City, @State, @Zip); SELECT CAST(SCOPE_IDENTITY() as int)", employee);
-            //insert the employee's dependents
-            employee.Dependents.ForEach(async dependent =>
+            try
             {
-                await connection.ExecuteAsync("INSERT INTO Dependent (DependentType, EmployeeId, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip)" +
-                "VALUES (@DependentType, @EmployeeId, @FirstName, @LastName, @Email, @PhoneNumber, @AddressLine1, @AddressLine2, @City, @State, @Zip); SELECT CAST(SCOPE_IDENTITY() as int)",
-                new { DependentType = dependent.DependentType, EmployeeId = result, FirstName = dependent.FirstName, LastName = dependent.LastName, Email = dependent.Email, PhoneNumber = dependent.PhoneNumber, AddressLine1 = dependent.AddressLine1,
-                AddressLine2 = dependent.AddressLine2, City = dependent.City, State = dependent.State, Zip = dependent.Zip});
-            });
+                //set up the connection
+                using var connection = new SqliteConnection($"Data Source={databaseConfig.Name}");
+                //insert the employee and get the id generated from it's insertion
+                var result = await connection.ExecuteScalarAsync<int>("INSERT INTO Employee (FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip)" +
+                    "VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @AddressLine1, @AddressLine2, @City, @State, @Zip); SELECT last_insert_rowid()", employee);
+                //insert the employee's dependents
+                employee.Dependents.ForEach(async dependent =>
+                {
+                    await connection.ExecuteAsync("INSERT INTO Dependent (DependentType, EmployeeId, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip)" +
+                    "VALUES (@DependentType, @EmployeeId, @FirstName, @LastName, @Email, @PhoneNumber, @AddressLine1, @AddressLine2, @City, @State, @Zip); SELECT last_insert_rowid()",
+                    new { DependentType = dependent.DependentType, EmployeeId = result, FirstName = dependent.FirstName, LastName = dependent.LastName, Email = dependent.Email, PhoneNumber = dependent.PhoneNumber, AddressLine1 = dependent.AddressLine1,
+                    AddressLine2 = dependent.AddressLine2, City = dependent.City, State = dependent.State, Zip = dependent.Zip});
+                });
 
-            //grab and return a list of all employees and their dependents to return to the client
-            var employees = (await connection.QueryAsync<Employee>("SELECT rowid as EmployeeId, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip FROM Employee")).AsList();
+                //grab and return a list of all employees and their dependents to return to the client
+                var employees = (await connection.QueryAsync<Employee>("SELECT rowid as EmployeeId, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip FROM Employee")).AsList();
 
-            //loop over each of the employees
-            employees.ForEach(async employee =>
+                //loop over each of the employees
+                employees.ForEach(async employee =>
+                {
+                    employee.Dependents = (await connection.QueryAsync<Dependent>("SELECT rowid as DependentId, DependentType, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip FROM Dependent WHERE EmployeeId = @EmployeeId", new { EmployeeId = employee.EmployeeId })).AsList();
+                });
+
+                //return the employees
+                return new EmployeeList() { Employees = employees };
+            }
+            catch(Exception e)
             {
-                employee.Dependents = (await connection.QueryAsync<Dependent>("SELECT rowid as DependentId, DependentType, FirstName, LastName, Email, PhoneNumber, AddressLine1, AddressLine2, City, State, Zip FROM Dependent WHERE EmployeeId = @EmployeeId", new { EmployeeId = employee.EmployeeId })).AsList();
-            });
-
-            //return the employees
-            return new EmployeeList() { Employees = employees };
+                var ex = e;
+                return new EmployeeList();
+            }
 
         }
         public async Task<EmployeeList> GetEmployeeList()
@@ -61,61 +69,6 @@ namespace employee_benefits_api.Services
 
             //return the employees
             return new EmployeeList() { Employees = employees };
-
-            /*
-            //create a temp list of employees
-            EmployeeList employeeList = new EmployeeList()
-            {
-                Employees = new List<Employee>()
-                {
-                    new Employee()
-                    {
-                        EmployeeId = 1,
-                        FirstName = "Pat",
-                        LastName = "Johnson",
-                        Email = "pjohnson@gmail.com",
-                        PhoneNumber = 5555555555,
-                        AddressLine1 = "123 Fourth St",
-                        AddressLine2 = "Apt. 9",
-                        City = "Crystal Lake",
-                        State = "Illinois",
-                        Zip = 60014,
-                        Dependents = new List<Dependent>()
-                        {
-                            new Dependent()
-                            {
-                                DependentId = 1,
-                                DependentType = "Spouse",
-                                FirstName = "Jaimie",
-                                LastName = "Johnson",
-                                Email = "jjohnson@gmail.com",
-                                PhoneNumber = 6666666666,
-                                AddressLine1 = "123 Fourth St",
-                                AddressLine2 = "Apt. 9",
-                                City = "Crystal Lake",
-                                State = "Illinois",
-                                Zip = 60014
-                            },
-                            new Dependent()
-                            {
-                                DependentId = 2,
-                                DependentType = "Child",
-                                FirstName = "Jimmy",
-                                LastName = "Johnson",
-                                Email = "",
-                                PhoneNumber = 0,
-                                AddressLine1 = "123 Fourth St",
-                                AddressLine2 = "Apt. 9",
-                                City = "Crystal Lake",
-                                State = "Illinois",
-                                Zip = 60014
-                            }
-                        }
-                    }
-                }
-            };
-            //return employeeList;
-            */
         }
     }
 }
